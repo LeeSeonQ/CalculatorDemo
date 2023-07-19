d9bf33cfb23dc59bc101128e402c247b82f14767
LeeSeonQ <lsk021181@gmail.com>
2023-07-19 18:55:53 +0900

    Lv3

--- NewCalculatorDemo.playground/Contents.swift
+++ NewCalculatorDemo.playground/Contents.swift
@@ -1,20 +1,26 @@
 import UIKit
 
 class Calculator {
+    
+    let addOperation = AddOperation()
+    let subtractOperation = SubtractOperation()
+    let multiplyOperation = MultiplyOperation()
+    let divideOperation = DivideOperation()
+    
     func calculate(operation: String, firstNum: Double, secondNum: Double) -> Double? {
         if operation == "+" {
-            return ( firstNum + secondNum )
+            return (addOperation.operate(firstNum: firstNum, secondNum: secondNum))
         } else if operation == "-" {
-            return ( firstNum - secondNum )
+            return (subtractOperation.operate(firstNum: firstNum, secondNum: secondNum))
         } else if operation == "*" {
-            return ( firstNum * secondNum )
+            return (multiplyOperation.operate(firstNum: firstNum, secondNum: secondNum))
         } else if operation == "/" {
             if secondNum == 0 {
                 return nil
             }
-            return ( firstNum / secondNum )
-//        } else if operation == "%" {
-//            return (firstNum % secondNum)
+            return (divideOperation.operate(firstNum: firstNum, secondNum: secondNum))
+        } else if operation == "%" {
+            return (firstNum.truncatingRemainder(dividingBy: secondNum))
         } else {
             return nil
         }
@@ -22,14 +28,30 @@ class Calculator {
         
     }
 
-
-
-
-
+class AddOperation {
+    func operate(firstNum: Double, secondNum: Double) -> Double{
+        return firstNum + secondNum
+    }
+}
+class SubtractOperation{
+    func operate(firstNum: Double, secondNum: Double) -> Double{
+        return firstNum - secondNum
+    }
+}
+class MultiplyOperation{
+    func operate(firstNum: Double, secondNum: Double) -> Double{
+        return firstNum * secondNum
+    }
+}
+class DivideOperation{
+    func operate(firstNum: Double, secondNum: Double) -> Double{
+        return firstNum / secondNum
+    }
+}
 let calculator = Calculator()
 
 let result1 = calculator.calculate(operation: "+", firstNum: 5, secondNum: 3)
 let result2 = calculator.calculate(operation: "-", firstNum: 5, secondNum: 3)
 let result3 = calculator.calculate(operation: "*", firstNum: 5, secondNum: 3)
 let result4 = calculator.calculate(operation: "/", firstNum: 6, secondNum: 3)
-//let result5 = calculator.calculate(operation: "%", firstNum: 5, secondNum: 3)
+let result5 = calculator.calculate(operation: "%", firstNum: 5, secondNum: 3)
