5dd3de36de9d46a4410133e55c68af5eaf5aa12b
LeeSeonQ <lsk021181@gmail.com>
2023-07-20 18:32:48 +0900

    Lv2 ~ Lv3

--- MyCalculator.playground/Contents.swift
+++ MyCalculator.playground/Contents.swift
@@ -1,18 +1,48 @@
 import UIKit
-// 사칙연산 클래스 만들고 연산 진행 출력.
+
+// Lv 3 ~
+class AddOperation {
+    func operate (n1: Double, n2:Double) -> Double {
+        return n1 + n2
+    }
+}
+class SubtractOperation {
+    func operate (n1: Double, n2:Double) -> Double {
+        return n1 - n2
+    }
+}
+class MultiplyOperation {
+    func operate (n1: Double, n2:Double) -> Double {
+        return n1 * n2
+    }
+}
+class DivdeOperation {
+    func operate (n1: Double, n2:Double) -> Double {
+        return n1 / n2
+    }
+}
+// ~ Lv 3
+
 class Calculator {
+    let addOperation = AddOperation()
+    let subtractOperation = SubtractOperation()
+    let multiplyOperation = MultiplyOperation()
+    let divideOperation = DivdeOperation()
+    
     func calculate (operation: String, n1: Double, n2: Double) -> Double? {
         if operation == "+" {
-            return n1 + n2
+            return (addOperation.operate(n1: n1, n2: n2))
         } else if operation == "-" {
-            return n1 - n2
+            return (subtractOperation.operate(n1: n1, n2: n2))
         } else if operation == "*" {
-            return n1 * n2
+            return (multiplyOperation.operate(n1: n1, n2: n2))
         } else if operation == "/" {
             if n2 == 0 {
                 return nil
             }
-            return n1 / n2
+            return (divideOperation.operate(n1: n1, n2: n2))
+        } else if operation == "%"{// Lv 2 ~
+          return (n1.truncatingRemainder(dividingBy: n2)) // ~ Lv 2
         } else {
             return nil
         }
@@ -20,5 +50,14 @@ class Calculator {
     }
 }
 
+
+
+
+
 let calculator = Calculator()
+
 let result1 = calculator.calculate (operation: "+", n1: 10, n2: 5)
+let result2 = calculator.calculate (operation: "-", n1: 10, n2: 5)
+let result3 = calculator.calculate (operation: "*", n1: 10, n2: 5)
+let result4 = calculator.calculate (operation: "/", n1: 10, n2: 5)
+let result5 = calculator.calculate (operation: "%", n1: 10, n2: 5)
